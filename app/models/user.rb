a0825ff76e14babf3a07bc243665ae22e71e4f92
devProjetsAlex <alexandre.cote.5@gmail.com>
2020-05-21 11:34:01 -0400

    bcrypt secure gem added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
-    has_many :reservations
+	before_save { self.email = email.downcase}
 
-    validates :username, presence: true, uniqueness: {case_sensitive: false }, length:{minimum:3, maximum:25}
+  has_many :reservations
+
+  validates :username, presence: true, uniqueness: {case_sensitive: false }, length:{minimum:3, maximum:25}
     
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-    validates :email, presence: true,
-    uniqueness: {case_sensitive: false },
-    length: {maximum:105},
-    format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true,
+  uniqueness: {case_sensitive: false },
+  length: {maximum:105},
+  format: { with: VALID_EMAIL_REGEX }
     
 end
