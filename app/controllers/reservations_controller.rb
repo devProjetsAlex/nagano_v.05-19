c53584b13889a70622d31c2adf13d03b0481436a
devProjetsAlex <alexandre.cote.5@gmail.com>
2020-05-21 14:10:23 -0400

    users edit reservation and edit profile

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,7 +3,7 @@ class ReservationsController < ApplicationController
     before_action :set_reservation, only: [:show, :edit, :update,:destroy]
   
       def show
-         
+       
       end
   
       def index
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,29 @@
 class UsersController < ApplicationController
     
+  def show
+    @user = User.find(params[:id])
+    @reservations = @user.reservations
+  end
+
   def new
     @user = User.new
   end
+
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "La modification de votre compte est terminée."
+      redirect_to reservations_path
+    else
+      render 'edit'
+    end
+  end
+
 
   def create
     @user = User.new(user_params)
